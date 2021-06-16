60c0c6ec6f4dcfd389a73c5090743b4dfd9136fb
CW-B-W <cw.brad.wang@gmail.com>
2021-06-16 15:54:30 +0800

    Add demo file

--- makefile
+++ makefile
@@ -4,20 +4,30 @@ help:
 	@printf '\n'
 	@printf 'options:\n'
 	@printf '\ttest\t\trun test\n'
+	@printf '\tdemo\t\trun demo\n'
 	@printf '\tclean\t\tclean compiled files\n'
 
-CC       = g++
-CXXFLAGS = -Wall -O3 --std=c++17
-DEPS     = src/FixedPointNumberTest.cpp include/FixedPointNumber.hpp
-SRCS     = src/FixedPointNumberTest.cpp
-INCLUDES = -I./include
+CC        = g++
+CXXFLAGS  = -Wall -O0 --std=c++17
+TEST_DEPS = src/FixedPointNumberTest.cpp include/FixedPointNumber.hpp
+TEST_SRCS = src/FixedPointNumberTest.cpp
+DEMO_DEPS = src/FixedPointNumberDemo.cpp include/FixedPointNumber.hpp
+DEMO_SRCS = src/FixedPointNumberDemo.cpp
+INCLUDES  = -I./include
 
 .PHONY: test
 test: bin/FixedPointNumberTest.out
 	bin/FixedPointNumberTest.out
 
-bin/FixedPointNumberTest.out: $(DEPS)
-	$(CC) -o $@ $(SRCS) $(INCLUDES) $(CXXFLAGS)
+bin/FixedPointNumberTest.out: $(TEST_DEPS)
+	$(CC) -o $@ $(TEST_SRCS) $(INCLUDES) $(CXXFLAGS)
+
+.PHONY: demo
+demo: bin/FixedPointNumberDemo.out
+	bin/FixedPointNumberDemo.out
+
+bin/FixedPointNumberDemo.out: $(DEMO_DEPS)
+	$(CC) -o $@ $(DEMO_SRCS) $(INCLUDES) $(CXXFLAGS)
 
 .PHONY: clean
 clean:
--- src/FixedPointNumberTest.cpp
+++ src/FixedPointNumberTest.cpp
@@ -6,188 +6,324 @@
 
 using namespace std;
 
-extern void test();
+template<int T1, int T2>
+class FixedPointNumberTest : public FixedPointNumber<T1, T2>
+{
+public:
+    FixedPointNumberTest() : FixedPointNumber<T1, T2>()
+    {
+        cout << "called ctor FixedPointNumber<" << T1 << ", " << T2 << ">" << "()" << endl;
+        print_info();
+    }
+    FixedPointNumberTest(uint32_t u32) : FixedPointNumber<T1, T2>(u32)
+    {
+        cout << "called ctor FixedPointNumber<" << T1 << ", " << T2 << ">" << "(uint32_t)" << endl;
+        print_info();
+    }
+    FixedPointNumberTest( int32_t i32) : FixedPointNumber<T1, T2>(i32)
+    {
+        cout << "called ctor FixedPointNumber<" << T1 << ", " << T2 << ">" << "(int32_t)" << endl;
+        print_info();
+    }
+    FixedPointNumberTest(double d)     : FixedPointNumber<T1, T2>(d)
+    {
+        cout << "called ctor FixedPointNumber<" << T1 << ", " << T2 << ">" << "(double)" << endl;
+        print_info();
+    }
+    template<int T3, int T4>
+    FixedPointNumberTest(const FixedPointNumber<T3, T4> &that) : FixedPointNumber<T1, T2>(that)
+    {
+        // cout << "called ctor FixedPointNumber<" << T1 << ", " << T2 << ">" << "(that)" << endl;
+        // print_info();
+    }
+
+    FixedPointNumberTest operator- ()
+    {
+        return FixedPointNumber<T1,T2>::operator-();
+    }
+
+    template<int T3, int T4>
+    FixedPointNumberTest operator* (const FixedPointNumberTest<T3, T4> &rhs)
+    {
+        FixedPointNumberTest& lhs = *this;
+        cout << "[Test operator*]" << endl;
+        cout << "FixedPointNumberTest<" << T1 << ", " << T2 << "> * FixedPointNumberTest<" << T3 << ", " << T4 << ">" << endl;
+        cout << "Result will be " << "FixedPointNumberTest<" << T1 << ", " << T2 << ">" << endl;
+        cout << '\t' << lhs.to_double() << " * " << rhs.to_double() << endl;
+        FixedPointNumberTest res = FixedPointNumber<T1, T2>::operator*(static_cast<FixedPointNumber<T3,T4>>(rhs));
+        cout << '\t' << "= " << res.to_double() << endl;
+        cout << '\t' << "Expected if full-precision" << endl;
+        cout << '\t' << "= " << lhs.to_double() * rhs.to_double() << endl;
+        cout << endl;
+        return res;
+    }
+
+    template<int T3, int T4>
+    FixedPointNumberTest operator- (const FixedPointNumberTest<T3, T4> &rhs)
+    {
+        FixedPointNumberTest& lhs = *this;
+        cout << "[Test operator-]" << endl;
+        cout << "FixedPointNumberTest<" << T1 << ", " << T2 << "> - FixedPointNumberTest<" << T3 << ", " << T4 << ">" << endl;
+        cout << "Result will be " << "FixedPointNumberTest<" << T1 << ", " << T2 << ">" << endl;
+        cout << '\t' << lhs.to_double() << " - " << rhs.to_double() << endl;
+        FixedPointNumberTest res = FixedPointNumber<T1, T2>::operator-(static_cast<FixedPointNumber<T3,T4>>(rhs));
+        cout << '\t' << "= " << res.to_double() << endl;
+        cout << '\t' << "Expected if full-precision" << endl;
+        cout << '\t' << "= " << lhs.to_double() - rhs.to_double() << endl;
+        cout << endl;
+        return res;
+    }
+
+    template<int T3, int T4>
+    FixedPointNumberTest operator+ (const FixedPointNumberTest<T3, T4> &rhs)
+    {
+        FixedPointNumberTest& lhs = *this;
+        cout << "[Test operator+]" << endl;
+        cout << "FixedPointNumberTest<" << T1 << ", " << T2 << "> + FixedPointNumberTest<" << T3 << ", " << T4 << ">" << endl;
+        cout << "Result will be " << "FixedPointNumberTest<" << T1 << ", " << T2 << ">" << endl;
+        cout << '\t' << lhs.to_double() << " + " << rhs.to_double() << endl;
+        FixedPointNumberTest res = FixedPointNumber<T1, T2>::operator+(static_cast<FixedPointNumber<T3,T4>>(rhs));
+        cout << '\t' << "= " << res.to_double() << endl;
+        cout << '\t' << "Expected if full-precision" << endl;
+        cout << '\t' << "= " << lhs.to_double() + rhs.to_double() << endl;
+        cout << endl;
+        return res;
+    }
+
+    void print_double()
+    {
+        cout << "[Print double]" << endl;
+        cout << '\t' << FixedPointNumber<T1, T2>::to_double() << endl;
+        cout << endl;
+    }
+
+    void print_value()
+    {
+        cout << "[Print value]" << endl;
+        cout << '\t' << static_cast<FixedPointNumber<T1,T2>>(*this) << endl;
+        cout << endl;
+    }
+
+    void print_info()
+    {
+        cout << "[Print info]" << endl;
+        cout << "\tdouble = " << FixedPointNumber<T1, T2>::to_double() << endl;
+        cout << "\tvalue  = " << static_cast<FixedPointNumber<T1,T2>>(*this) << endl;
+        cout << endl;
+    }
+};
+
+void print_title(string s, int lvl)
+{
+    constexpr int line_char_num = 60;
+    constexpr char char_list[4] = {'=', '-', '*', '%'};
+    char pad_char = char_list[lvl];
+    int side_pad_num = (line_char_num - s.size() - 2)/2;
+    cout << string(side_pad_num, pad_char) << " ";
+    cout << s;
+    cout << " " << string(side_pad_num - !(s.size()&1), pad_char);
+    cout << endl;
+}
 
 int main()
 {
-    cout << setprecision(10);
+    print_title("Custom test", 0);
+    FixedPointNumberTest<15, 16>  x = 0.0078125;
+    FixedPointNumberTest<15, 16>  h = -0.000427246;
+    x.print_double();
+    h.print_double();
+    (x*h).print_info();
+    (x).print_info();
+    (-h).print_info();
+    ((x)*(-h)).print_info();
+    print_title("End of Custom test", 0);
 
-    cout << "-----0-----" << endl;
-    FixedPointNumber<15, 16> fp;
-    fp = FixedPointNumber<15, 16>(9999.9999);
-    fp = FixedPointNumber<15, 16>(0x12345678);
-    fp = FixedPointNumber<15, 16>(0x12345678U);
-    fp = FixedPointNumber<15, 16>(FixedPointNumber<7,8>(127.127127));
-    cout << fp << endl;
-    cout << fp.to_double() << endl;
-    cout << fp.get_value() << endl;
-    cout << (-fp).to_double() << endl;
-    cout << (fp+fp).to_double() << endl;
-    cout << (fp*fp).to_double() << endl;
-    cout << "-----0.1-----" << endl;
-    fp = 1.23;
-    FixedPointNumber<7, 8> fph = fp;
-    fph = fp;
-    cout << fph.to_double() << endl;
-    cout << "-----0.2-----" << endl;
-    FixedPointNumber<7, 8> fpm1 = 0x10FF;
-    FixedPointNumber<15, 16> fpm2 = 0x0000FFFF;
-    cout << fpm1.to_double() << " * " << fpm2.to_double() << endl;
-    cout << (fpm1 * fpm2).to_double() << endl;
-    cout << fpm2.to_double() << " * " << fpm1.to_double() << endl;
-    cout << (fpm2 * fpm1).to_double() << endl;
+    print_title("Test 0 - Basic operations", 0);
+
+    print_title("Test constructors", 1);
+    FixedPointNumberTest<15, 16> fp;
+    fp = FixedPointNumberTest<15, 16>(9999.9999);
+    fp = FixedPointNumberTest<15, 16>(0x12345678);
+    fp = FixedPointNumberTest<15, 16>(0x12345678U);
+    fp = FixedPointNumberTest<15, 16>(FixedPointNumberTest<7,8>(127.127127));
+    print_title("Test functions", 1);
+    fp.print_value();
+    fp.print_double();
+    (-fp);
+    (fp+fp);
+    (fp-fp);
+    (fp*fp);
+
+    print_title("End of Test 0", 0);
     cout << endl;
+    
 
 
-    cout << "-----1-----" << endl;
-    FixedPointNumber<3, 16> t1(0xFFF9EU);
-    FixedPointNumber<7, 8>  t2(0x0067);
-    FixedPointNumber<7, 8>  t3(0x008F);
-    cout << t1.to_double() << endl;
-    cout << t2.to_double() << endl;
-    cout << t3.to_double() << endl;
-    cout << endl;
+    print_title("Test 1 - Op with two diff length fp", 0);
 
-    cout << "-----2-----" << endl;
-    cout << FixedPointNumber<3, 16>::PMAX_DOUBLE << endl;
-    cout << FixedPointNumber<3, 16>::PMIN_DOUBLE << endl;
-    cout << FixedPointNumber<3, 16>::NMAX_DOUBLE << endl;
-    cout << FixedPointNumber<3, 16>::NMIN_DOUBLE << endl;
-    cout << FixedPointNumber<3, 16>(999.999) << endl;
-    cout << FixedPointNumber<3, 16>(0.00000000001) << endl;
-    cout << FixedPointNumber<3, 16>(-999.999) << endl;
-    cout << FixedPointNumber<3, 16>(-0.00000000001) << endl;
-    cout << endl;
+    FixedPointNumberTest<7, 8> fpm1 = 0x10FF;
+    FixedPointNumberTest<15, 16> fpm2 = 0x0000FFFF;
+    (fpm1 * fpm2);
+    (fpm2 * fpm1);
 
-    cout << "-----3-----" << endl;
-    cout << (FixedPointNumber<3, 16>(FixedPointNumber<7, 8>(1.2345678))) << endl;
-    cout << (FixedPointNumber<3, 16>(FixedPointNumber<7, 8>(1.2345678))).to_double() << endl;
-    cout << (FixedPointNumber<7, 8>(FixedPointNumber<3, 16>(1.2345678))) << endl;
-    cout << (FixedPointNumber<7, 8>(FixedPointNumber<3, 16>(1.2345678))).to_double() << endl;
-    cout << endl;
-    cout << "-----4-----" << endl;
-    cout << (FixedPointNumber<3, 16>(FixedPointNumber<7, 8>(-1.2345678))) << endl;
-    cout << (FixedPointNumber<3, 16>(FixedPointNumber<7, 8>(-1.2345678))).to_double() << endl;
-    cout << (FixedPointNumber<7, 8>(FixedPointNumber<3, 16>(-1.2345678))) << endl;
-    cout << (FixedPointNumber<7, 8>(FixedPointNumber<3, 16>(-1.2345678))).to_double() << endl;
-    cout << endl;
-    cout << "-----5-----" << endl;
-    cout << (FixedPointNumber<3, 16>(1.2345678) * FixedPointNumber<7, 8>(1.234)).to_double() << endl;
-    cout << (FixedPointNumber<7, 8>(1.234) * FixedPointNumber<3, 16>(1.2345678)).to_double() << endl;
-    cout << endl;
-
-    FixedPointNumber<3, 16> fp1;
-    FixedPointNumber<3, 16> fp2;
-
-    cout << "-----6-----" << endl;
-    fp1 = FixedPointNumber<3, 16>(1.2);
-    fp2 = FixedPointNumber<3, 16>(3.4);
-    cout << fp1 << endl;
-    cout << fp2 << endl;
-    cout << fp1.to_double() << endl;
-    cout << fp2.to_double() << endl;
-    cout << (-fp1).to_double() << endl;
-    cout << (-fp2).to_double() << endl;
-    cout << (fp1+fp2).to_double() << endl;
-    cout << (fp1-fp2).to_double() << endl;
-    cout << (fp1*fp2).to_double() << endl;
-    cout << endl;
-
-    fp1 = FixedPointNumber<3, 16>(1.2);
-    fp2 = FixedPointNumber<3, 16>(-3.4);
-    cout << fp1 << endl;
-    cout << fp2 << endl;
-    cout << fp1.to_double() << endl;
-    cout << fp2.to_double() << endl;
-    cout << (-fp1).to_double() << endl;
-    cout << (-fp2).to_double() << endl;
-    cout << (fp1+fp2).to_double() << endl;
-    cout << (fp1-fp2).to_double() << endl;
-    cout << (fp1*fp2).to_double() << endl;
-    cout << endl;
-
-    fp1 = FixedPointNumber<3, 16>(-1.2);
-    fp2 = FixedPointNumber<3, 16>(3.4);
-    cout << fp1 << endl;
-    cout << fp2 << endl;
-    cout << fp1.to_double() << endl;
-    cout << fp2.to_double() << endl;
-    cout << (-fp1).to_double() << endl;
-    cout << (-fp2).to_double() << endl;
-    cout << (fp1+fp2).to_double() << endl;
-    cout << (fp1-fp2).to_double() << endl;
-    cout << (fp1*fp2).to_double() << endl;
-    cout << endl;
-
-    fp1 = FixedPointNumber<3, 16>(-1.2);
-    fp2 = FixedPointNumber<3, 16>(-3.4);
-    cout << fp1 << endl;
-    cout << fp2 << endl;
-    cout << fp1.to_double() << endl;
-    cout << fp2.to_double() << endl;
-    cout << (-fp1).to_double() << endl;
-    cout << (-fp2).to_double() << endl;
-    cout << (fp1+fp2).to_double() << endl;
-    cout << (fp1-fp2).to_double() << endl;
-    cout << (fp1*fp2).to_double() << endl;
+    print_title("End of Test 1", 0);
     cout << endl;
 
 
 
-    FixedPointNumber<7, 8> fp3;
-    FixedPointNumber<7, 8> fp4;
+    print_title("Test 2 - Overflow test", 0);
 
-    fp3 = FixedPointNumber<7, 8>(1.2);
-    fp4 = FixedPointNumber<7, 8>(3.4);
-    cout << fp3 << endl;
-    cout << fp4 << endl;
-    cout << fp3.to_double() << endl;
-    cout << fp4.to_double() << endl;
-    cout << (-fp3).to_double() << endl;
-    cout << (-fp4).to_double() << endl;
-    cout << (fp3+fp4).to_double() << endl;
-    cout << (fp3-fp4).to_double() << endl;
-    cout << (fp3*fp4).to_double() << endl;
+    FixedPointNumberTest<3, 16> fpof;
+    print_title("Max/Min of FixedPointNumberTest<3, 16>", 1);
+    cout << "Positive Max of FixedPointNumberTest<3, 16> = " << fpof.PMAX_DOUBLE << endl;
+    cout << "Positive Min of FixedPointNumberTest<3, 16> = " << fpof.PMIN_DOUBLE << endl;
+    cout << "Negative Max of FixedPointNumberTest<3, 16> = " << fpof.NMAX_DOUBLE << endl;
+    cout << "Negative Min of FixedPointNumberTest<3, 16> = " << fpof.NMIN_DOUBLE << endl;
+    print_title("Set greater than Positive Max", 1);
+    fpof = 999.999;
+    fpof.print_value();
+    print_title("Set less than Positive Max", 1);
+    fpof = 0.00000000001;
+    fpof.print_value();
+    print_title("Set greater than Negative Max", 1);
+    fpof = -0.00000000001;
+    fpof.print_value();
+    print_title("Set less than Negative Min", 1);
+    fpof = -999.999;
+    fpof.print_value();
+
+    print_title("End of Test 2", 0);
     cout << endl;
 
-    fp3 = FixedPointNumber<7, 8>(1.2);
-    fp4 = FixedPointNumber<7, 8>(-3.4);
-    cout << fp3 << endl;
-    cout << fp4 << endl;
-    cout << fp3.to_double() << endl;
-    cout << fp4.to_double() << endl;
-    cout << (-fp3).to_double() << endl;
-    cout << (-fp4).to_double() << endl;
-    cout << (fp3+fp4).to_double() << endl;
-    cout << (fp3-fp4).to_double() << endl;
-    cout << (fp3*fp4).to_double() << endl;
-    cout << endl;
+    // cout << "-----3-----" << endl;
+    // cout << (FixedPointNumber<3, 16>(FixedPointNumber<7, 8>(1.2345678))) << endl;
+    // cout << (FixedPointNumber<3, 16>(FixedPointNumber<7, 8>(1.2345678))).to_double() << endl;
+    // cout << (FixedPointNumber<7, 8>(FixedPointNumber<3, 16>(1.2345678))) << endl;
+    // cout << (FixedPointNumber<7, 8>(FixedPointNumber<3, 16>(1.2345678))).to_double() << endl;
+    // cout << endl;
+    // cout << "-----4-----" << endl;
+    // cout << (FixedPointNumber<3, 16>(FixedPointNumber<7, 8>(-1.2345678))) << endl;
+    // cout << (FixedPointNumber<3, 16>(FixedPointNumber<7, 8>(-1.2345678))).to_double() << endl;
+    // cout << (FixedPointNumber<7, 8>(FixedPointNumber<3, 16>(-1.2345678))) << endl;
+    // cout << (FixedPointNumber<7, 8>(FixedPointNumber<3, 16>(-1.2345678))).to_double() << endl;
+    // cout << endl;
+    // cout << "-----5-----" << endl;
+    // cout << (FixedPointNumber<3, 16>(1.2345678) * FixedPointNumber<7, 8>(1.234)).to_double() << endl;
+    // cout << (FixedPointNumber<7, 8>(1.234) * FixedPointNumber<3, 16>(1.2345678)).to_double() << endl;
+    // cout << endl;
 
-    fp3 = FixedPointNumber<7, 8>(-1.2);
-    fp4 = FixedPointNumber<7, 8>(3.4);
-    cout << fp3 << endl;
-    cout << fp4 << endl;
-    cout << fp3.to_double() << endl;
-    cout << fp4.to_double() << endl;
-    cout << (-fp3).to_double() << endl;
-    cout << (-fp4).to_double() << endl;
-    cout << (fp3+fp4).to_double() << endl;
-    cout << (fp3-fp4).to_double() << endl;
-    cout << (fp3*fp4).to_double() << endl;
-    cout << endl;
+    // FixedPointNumber<3, 16> fp1;
+    // FixedPointNumber<3, 16> fp2;
 
-    fp3 = FixedPointNumber<7, 8>(-1.2);
-    fp4 = FixedPointNumber<7, 8>(-3.4);
-    cout << fp3 << endl;
-    cout << fp4 << endl;
-    cout << fp3.to_double() << endl;
-    cout << fp4.to_double() << endl;
-    cout << (-fp3).to_double() << endl;
-    cout << (-fp4).to_double() << endl;
-    cout << (fp3+fp4).to_double() << endl;
-    cout << (fp3-fp4).to_double() << endl;
-    cout << (fp3*fp4).to_double() << endl;
-    cout << endl;
+    // cout << "-----6-----" << endl;
+    // fp1 = FixedPointNumber<3, 16>(1.2);
+    // fp2 = FixedPointNumber<3, 16>(3.4);
+    // cout << fp1 << endl;
+    // cout << fp2 << endl;
+    // cout << fp1.to_double() << endl;
+    // cout << fp2.to_double() << endl;
+    // cout << (-fp1).to_double() << endl;
+    // cout << (-fp2).to_double() << endl;
+    // cout << (fp1+fp2).to_double() << endl;
+    // cout << (fp1-fp2).to_double() << endl;
+    // cout << (fp1*fp2).to_double() << endl;
+    // cout << endl;
+
+    // fp1 = FixedPointNumber<3, 16>(1.2);
+    // fp2 = FixedPointNumber<3, 16>(-3.4);
+    // cout << fp1 << endl;
+    // cout << fp2 << endl;
+    // cout << fp1.to_double() << endl;
+    // cout << fp2.to_double() << endl;
+    // cout << (-fp1).to_double() << endl;
+    // cout << (-fp2).to_double() << endl;
+    // cout << (fp1+fp2).to_double() << endl;
+    // cout << (fp1-fp2).to_double() << endl;
+    // cout << (fp1*fp2).to_double() << endl;
+    // cout << endl;
+
+    // fp1 = FixedPointNumber<3, 16>(-1.2);
+    // fp2 = FixedPointNumber<3, 16>(3.4);
+    // cout << fp1 << endl;
+    // cout << fp2 << endl;
+    // cout << fp1.to_double() << endl;
+    // cout << fp2.to_double() << endl;
+    // cout << (-fp1).to_double() << endl;
+    // cout << (-fp2).to_double() << endl;
+    // cout << (fp1+fp2).to_double() << endl;
+    // cout << (fp1-fp2).to_double() << endl;
+    // cout << (fp1*fp2).to_double() << endl;
+    // cout << endl;
+
+    // fp1 = FixedPointNumber<3, 16>(-1.2);
+    // fp2 = FixedPointNumber<3, 16>(-3.4);
+    // cout << fp1 << endl;
+    // cout << fp2 << endl;
+    // cout << fp1.to_double() << endl;
+    // cout << fp2.to_double() << endl;
+    // cout << (-fp1).to_double() << endl;
+    // cout << (-fp2).to_double() << endl;
+    // cout << (fp1+fp2).to_double() << endl;
+    // cout << (fp1-fp2).to_double() << endl;
+    // cout << (fp1*fp2).to_double() << endl;
+    // cout << endl;
+
+
+
+    // FixedPointNumber<7, 8> fp3;
+    // FixedPointNumber<7, 8> fp4;
+
+    // fp3 = FixedPointNumber<7, 8>(1.2);
+    // fp4 = FixedPointNumber<7, 8>(3.4);
+    // cout << fp3 << endl;
+    // cout << fp4 << endl;
+    // cout << fp3.to_double() << endl;
+    // cout << fp4.to_double() << endl;
+    // cout << (-fp3).to_double() << endl;
+    // cout << (-fp4).to_double() << endl;
+    // cout << (fp3+fp4).to_double() << endl;
+    // cout << (fp3-fp4).to_double() << endl;
+    // cout << (fp3*fp4).to_double() << endl;
+    // cout << endl;
+
+    // fp3 = FixedPointNumber<7, 8>(1.2);
+    // fp4 = FixedPointNumber<7, 8>(-3.4);
+    // cout << fp3 << endl;
+    // cout << fp4 << endl;
+    // cout << fp3.to_double() << endl;
+    // cout << fp4.to_double() << endl;
+    // cout << (-fp3).to_double() << endl;
+    // cout << (-fp4).to_double() << endl;
+    // cout << (fp3+fp4).to_double() << endl;
+    // cout << (fp3-fp4).to_double() << endl;
+    // cout << (fp3*fp4).to_double() << endl;
+    // cout << endl;
+
+    // fp3 = FixedPointNumber<7, 8>(-1.2);
+    // fp4 = FixedPointNumber<7, 8>(3.4);
+    // cout << fp3 << endl;
+    // cout << fp4 << endl;
+    // cout << fp3.to_double() << endl;
+    // cout << fp4.to_double() << endl;
+    // cout << (-fp3).to_double() << endl;
+    // cout << (-fp4).to_double() << endl;
+    // cout << (fp3+fp4).to_double() << endl;
+    // cout << (fp3-fp4).to_double() << endl;
+    // cout << (fp3*fp4).to_double() << endl;
+    // cout << endl;
+
+    // fp3 = FixedPointNumber<7, 8>(-1.2);
+    // fp4 = FixedPointNumber<7, 8>(-3.4);
+    // cout << fp3 << endl;
+    // cout << fp4 << endl;
+    // cout << fp3.to_double() << endl;
+    // cout << fp4.to_double() << endl;
+    // cout << (-fp3).to_double() << endl;
+    // cout << (-fp4).to_double() << endl;
+    // cout << (fp3+fp4).to_double() << endl;
+    // cout << (fp3-fp4).to_double() << endl;
+    // cout << (fp3*fp4).to_double() << endl;
+    // cout << endl;
     
     return 0;
 }
